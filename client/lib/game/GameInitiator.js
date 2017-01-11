74328cfd76cc37b9444d6033379d3dd976f7bfc5
clesquir <clesquir@devalto.com>
2017-01-10 21:40:53 -0500

    Removed db calls from updateGame

--- client/lib/game/GameInitiator.js
+++ client/lib/game/GameInitiator.js
@@ -12,29 +12,76 @@ export default class GameInitiator {
 		this.gameId = gameId;
 		this.streamInitiator = new StreamInitiator(this);
 		this.currentGame = null;
+		this.gameStartedAt = 0;
+		this.gameLastPointAt = 0;
+		this.gameStatus = null;
 		this.timerUpdater = null;
 		this.serverOffset = TimeSync.serverOffset();
 	}
 
 	init() {
-		let game = this.fetchGame();
 		let player = Players.findOne({gameId: this.gameId, userId: Meteor.userId()});
 
-		this.gameCreatedBy = game.createdBy;
-		this.currentGame = null;
+		this.updateGameProperties();
 
 		//Player is in game and this game is already started
-		if (player && isGameStatusOnGoing(game.status)) {
+		if (player && isGameStatusOnGoing(this.gameStatus)) {
 			this.createNewGame();
 		}
 
 		this.initTimer();
 
 		this.streamInitiator.init();
+
+		this.gamePointsTracker = Games.find({_id: this.gameId}).observeChanges({
+			changed: (id, fields) => {
+				let needsToUpdateGameProperties = false;
+
+				if (fields.hasOwnProperty('status')) {
+					needsToUpdateGameProperties = true;
+					if (this.hasActiveGame()) {
+						this.currentGame.onStatusChange();
+					}
+				}
+
+				if (fields.hasOwnProperty('activeBonuses')) {
+					if (this.hasActiveGame()) {
+						this.currentGame.onActiveBonusesChange();
+					}
+				}
+
+				if (fields.hasOwnProperty('lastPointAt')) {
+					needsToUpdateGameProperties = true;
+				}
+
+				if (needsToUpdateGameProperties) {
+					this.updateGameProperties();
+				}
+
+				if (
+					fields.hasOwnProperty(Constants.HOST_POINTS_COLUMN) ||
+					fields.hasOwnProperty(Constants.CLIENT_POINTS_COLUMN)
+				) {
+					if (this.hasActiveGame()) {
+						this.updateTimer();
+						this.currentGame.onPointTaken();
+					}
+				}
+			}
+		});
 	}
 
 	fetchGame() {
 		return Games.findOne({_id: this.gameId});
+	}
+
+	updateGameProperties() {
+		let game = this.fetchGame();
+
+		this.gameCreatedBy = game.createdBy;
+		this.gameStartedAt = game.lastPointAt;
+		this.gameLastPointAt = game.lastPointAt;
+		this.gameStatus = game.status;
 	}
 
 	userIsGameCreator() {
@@ -50,6 +97,10 @@ export default class GameInitiator {
 		this.streamInitiator.stop();
 
 		this.clearTimer();
+
+		if (this.gamePointsTracker) {
+			this.gamePointsTracker.stop();
+		}
 	}
 
 	createNewGame() {
@@ -64,15 +115,13 @@ export default class GameInitiator {
 	}
 
 	updateTimer() {
-		let game = this.fetchGame();
-
-		if (isGameStatusStarted(game.status)) {
-			let matchTimer = getUTCTimeStamp() + this.serverOffset - game.startedAt;
+		if (isGameStatusStarted(this.gameStatus)) {
+			let matchTimer = getUTCTimeStamp() + this.serverOffset - this.gameStartedAt;
 			if (matchTimer < 0) {
 				matchTimer = 0;
 			}
 
-			let pointTimer = getUTCTimeStamp() + this.serverOffset - game.lastPointAt;
+			let pointTimer = getUTCTimeStamp() + this.serverOffset - this.gameLastPointAt;
 			if (pointTimer < 0) {
 				pointTimer = 0;
 			}
