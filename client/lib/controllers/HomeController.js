b370272ddac8163b950f9574df560dff1baee2f2
clesquir <clesquir@devalto.com>
2017-02-19 15:49:19 -0500

    Fix longest point fetch even if no user

--- client/lib/controllers/HomeController.js
+++ client/lib/controllers/HomeController.js
@@ -22,16 +22,19 @@ HomeController = RouteController.extend({
 	},
 	action: function () {
 		Meteor.subscribe('recentProfileGames', this.gamesLimit());
-		Meteor.call('longestGame', function(error, data) {
-			if (!error && data.gameId) {
-				Session.set('longestGame', data);
-			}
-		});
-		Meteor.call('longestPoint', function(error, data) {
-			if (!error && data.gameId) {
-				Session.set('longestPoint', data);
-			}
-		});
+
+		if (Meteor.userId()) {
+			Meteor.call('longestGame', function(error, data) {
+				if (!error && data.gameId) {
+					Session.set('longestGame', data);
+				}
+			});
+			Meteor.call('longestPoint', function(error, data) {
+				if (!error && data.gameId) {
+					Session.set('longestPoint', data);
+				}
+			});
+		}
 
 		if (this.ready()) {
 			this.render();
