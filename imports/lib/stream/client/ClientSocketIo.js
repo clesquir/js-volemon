b26c9873069768669f8121db381cae30bc1beb81
clesquir <clesquir@devalto.com>
2017-02-23 22:31:46 -0500

    Increase maximum clients and make sure it is auto upgraded

--- imports/lib/stream/client/ClientSocketIo.js
+++ imports/lib/stream/client/ClientSocketIo.js
@@ -11,10 +11,10 @@ export default class ClientSocketIo extends Stream {
 		}
 
 		this.socket = require('socket.io-client').connect(url);
-		this.p2p = new (require('socket.io-p2p'))(this.socket);
+		this.p2p = new (require('socket.io-p2p'))(this.socket, {numClients: 10, autoUpgrade: true});
 		this.p2p.on('ready', () => {
 			this.p2p.usePeerConnection = true;
-		})
+		});
 	}
 
 	emit(eventName, payload) {
