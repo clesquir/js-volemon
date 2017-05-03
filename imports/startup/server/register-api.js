7be7d7093653537e47979c5a824fd66d23c6ff47
clesquir <clesquir@devalto.com>
2017-05-03 08:13:07 -0400

    Refactor and stats
    - register-api.js

---
+++ imports/startup/server/register-api.js
@@ -0,0 +1,7 @@
+import '/imports/api/home/server/methods.js';
+import '/imports/api/home/server/publications.js';
+import '/imports/api/profiles/server/profileCreation.js';
+import '/imports/api/users/server/methods.js';
+
+//Migrations
+import '/imports/api/profiles/server/migrations/noShutouts.js';
