e7c593250399172aff8012cd55f0f1eb3f338f4f
Cédric Lesquir <clesquir@devalto.com>
2017-05-21 19:54:50 -0400

    Update achievements levels (#83)
    - Updated initial update
    - Updated levels for pauses/invisibles in a point/game
    - Removed executed user profiles migrations (shutouts won and lost)
    - Isolated all migrations in a js file

---
+++ imports/startup/server/migrations.js
@@ -0,0 +1,3 @@
+import '/imports/api/achievements/server/migrations/initialAchievements.js';
+import '/imports/api/achievements/server/migrations/updateAchievementsLevels.js';
+import '/imports/api/achievements/server/migrations/userInitialAchievements.js';
