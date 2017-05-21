4eee231984b589316d736458b8596fb36579c869
clesquir <clesquir@devalto.com>
2017-05-21 19:54:25 -0400

    Update achievements levels
    - Updated initial update
    - Updated levels for pauses/invisibles in a point/game
    - Removed executed user profiles migrations (shutouts won and lost)
    - Isolated all migrations in a js file

---
+++ imports/api/achievements/server/migrations/updateAchievementsLevels.js
@@ -0,0 +1,45 @@
+import {Meteor} from 'meteor/meteor';
+import {Achievements} from '/imports/api/achievements/achievements.js';
+import {
+	ACHIEVEMENT_PAUSE_IN_A_POINT,
+	ACHIEVEMENT_PAUSE_IN_A_GAME,
+	ACHIEVEMENT_INVISIBLE_IN_A_POINT,
+	ACHIEVEMENT_INVISIBLE_IN_A_GAME
+} from '/imports/api/achievements/constants.js'
+
+Meteor.startup(function () {
+	/**
+	 * Migration for updating levels for pauses/invisibles in a point/game
+	 */
+	const pauseInAPoint = Achievements.findOne({_id: ACHIEVEMENT_PAUSE_IN_A_POINT});
+	if (pauseInAPoint !== undefined && pauseInAPoint.levels[0].number === 3) {
+		Achievements.update(
+			{_id: ACHIEVEMENT_PAUSE_IN_A_POINT},
+			{$set: {levels: [{"level": 1, "number": 2}, {"level": 2, "number": 4}, {"level": 3, "number": 6}]}}
+		);
+	}
+
+	const pauseInAGame = Achievements.findOne({_id: ACHIEVEMENT_PAUSE_IN_A_GAME});
+	if (pauseInAGame !== undefined && pauseInAGame.levels[0].number === 5) {
+		Achievements.update(
+			{_id: ACHIEVEMENT_PAUSE_IN_A_GAME},
+			{$set: {levels: [{"level": 1, "number": 3}, {"level": 2, "number": 6}, {"level": 3, "number": 9}]}}
+		);
+	}
+
+	const invisibleInAPoint = Achievements.findOne({_id: ACHIEVEMENT_INVISIBLE_IN_A_POINT});
+	if (invisibleInAPoint !== undefined && invisibleInAPoint.levels[0].number === 3) {
+		Achievements.update(
+			{_id: ACHIEVEMENT_INVISIBLE_IN_A_POINT},
+			{$set: {levels: [{"level": 1, "number": 3}, {"level": 2, "number": 6}, {"level": 3, "number": 9}]}}
+		);
+	}
+
+	const invisibleInAGame = Achievements.findOne({_id: ACHIEVEMENT_INVISIBLE_IN_A_GAME});
+	if (invisibleInAGame !== undefined && invisibleInAGame.levels[0].number === 5) {
+		Achievements.update(
+			{_id: ACHIEVEMENT_INVISIBLE_IN_A_GAME},
+			{$set: {levels: [{"level": 1, "number": 4}, {"level": 2, "number": 8}, {"level": 3, "number": 12}]}}
+		);
+	}
+});
--- imports/startup/server/register-api.js
+++ imports/startup/server/register-api.js
@@ -3,9 +3,4 @@ import '/imports/api/home/server/methods.js';
 import '/imports/api/home/server/publications.js';
 import '/imports/api/profiles/server/profileCreation.js';
 import '/imports/api/users/server/methods.js';
-
-//Migrations
-import '/imports/api/achievements/server/migrations/initialAchievements.js';
-import '/imports/api/profiles/server/migrations/numberOfShutouts.js';
-import '/imports/api/profiles/server/migrations/numberOfShutoutLosses.js';
-import '/imports/api/achievements/server/migrations/userInitialAchievements.js';
+import '/imports/startup/server/migrations.js';
