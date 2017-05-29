5e15b85f3a5fd8b45e08cb782b8ba39ce1a963eb
clesquir <clesquir@devalto.com>
2017-05-29 18:51:34 -0400

    Removed installed migrations

--- imports/startup/server/migrations.js
+++ imports/startup/server/migrations.js
@@ -1,6 +1,3 @@
 import '/imports/api/achievements/server/migrations/initialAchievements.js';
 import '/imports/api/achievements/server/migrations/initialUserAchievements.js';
-import '/imports/api/achievements/server/migrations/updateAchievementsLevels.js';
-import '/imports/api/achievements/server/migrations/updateAchievementsDisplayOrder.js';
 import '/imports/api/achievements/server/migrations/additionalAchievements.js';
-import '/imports/api/achievements/server/migrations/updateUserAchievementsGamesPlayedAndShutouts.js';
