83a941810537730a09029dc8c72954434eccd6f2
clesquir <clesquir@devalto.com>
2017-06-06 08:06:10 -0400

    Additional weird shapes

--- imports/api/games/shapeConstants.js
+++ imports/api/games/shapeConstants.js
@@ -2,6 +2,8 @@ export const PLAYER_SHAPE_HALF_CIRCLE = 'half-circle';
 export const PLAYER_SHAPE_TRIANGLE = 'triangle';
 export const PLAYER_SHAPE_X = 'x';
 export const PLAYER_SHAPE_RECTANGLE = 'rectangle';
+export const PLAYER_SHAPE_EQUAL = 'equal';
+export const PLAYER_SHAPE_CROWN = 'crown';
 export const PLAYER_SHAPE_RHOMBUS = 'rhombus';
 export const PLAYER_SHAPE_HEXAGON = 'hexagon';
 export const PLAYER_SHAPE_ELLIPSE = 'ellipse';
@@ -11,6 +13,8 @@ export const PLAYER_LIST_OF_SHAPES = [
 	PLAYER_SHAPE_TRIANGLE,
 	PLAYER_SHAPE_X,
 	PLAYER_SHAPE_RECTANGLE,
+	PLAYER_SHAPE_EQUAL,
+	PLAYER_SHAPE_CROWN,
 	PLAYER_SHAPE_RHOMBUS,
 	PLAYER_SHAPE_HEXAGON,
 	PLAYER_SHAPE_ELLIPSE
