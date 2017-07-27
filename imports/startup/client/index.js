6a1cc06c84a2572208364d20a6852fc401c5d35f
clesquir <clesquir@devalto.com>
2017-07-26 21:57:51 -0400

    Mobile controls and mobile improvements

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -3,3 +3,16 @@ import '/imports/lib/override/socket.io-client.js';
 import './routes.js';
 import './connectionMonitor.js';
 import './register-api.js';
+import {onMobileAndTablet} from '/imports/lib/utils.js';
+
+if (onMobileAndTablet()) {
+	const hideMobileAddressBar = function() {
+		setTimeout(function(){
+			//Hides the address bar!
+			window.scrollTo(0, 1);
+		}, 0);
+	};
+
+	window.addEventListener('load', hideMobileAddressBar);
+	window.addEventListener('orientationchange', hideMobileAddressBar);
+}
