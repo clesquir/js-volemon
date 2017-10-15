4ca29d9ab947bad809cb923635364f8422113997
clesquir <clesquir@devalto.com>
2017-10-15 12:30:49 -0400

    Limited time tournaments
     - Only one tournament for next week instead

--- imports/api/tournaments/server/migrations/initialTournaments.js
+++ imports/api/tournaments/server/migrations/initialTournaments.js
@@ -2,8 +2,7 @@ import {Meteor} from 'meteor/meteor';
 import {Random} from 'meteor/random';
 import {Tournaments} from '/imports/api/tournaments/tournaments.js';
 import {
-	TOURNAMENT_MODE_CLASSIC,
-	TOURNAMENT_MODE_HARDCORE
+	TOURNAMENT_MODE_CLASSIC
 } from '/imports/api/tournaments/tournamentModesConstants.js';
 import {TournamentModes} from '/imports/api/tournaments/tournamentModes.js';
 
@@ -11,15 +10,8 @@ Meteor.startup(function () {
 	const tournaments = [
 		{
 			"_id": Random.id(5),
-			"identifier": 'tournament_2017-10-16',
-			"mode": TOURNAMENT_MODE_CLASSIC,
-			"startDate": "2017-10-16 +0000",
-			"endDate": "2017-10-21 +0000"
-		},
-		{
-			"_id": Random.id(5),
 			"identifier": 'tournament_2017-10-23',
-			"mode": TOURNAMENT_MODE_HARDCORE,
+			"mode": TOURNAMENT_MODE_CLASSIC,
 			"startDate": "2017-10-23 +0000",
 			"endDate": "2017-10-28 +0000"
 		}
