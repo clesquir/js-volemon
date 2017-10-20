23bb71ec06d1bb41e5da72160017c5b162327fa3
clesquir <clesquir@devalto.com>
2017-10-20 08:46:12 -0400

    Improved server side timer
     - average of all serverOffset instead of strict response

--- imports/api/games/client/ServerNormalizedTime.js
+++ imports/api/games/client/ServerNormalizedTime.js
@@ -4,10 +4,13 @@ import {getUTCTimeStamp} from '/imports/lib/utils.js';
 
 export default class ServerNormalizedTime {
 	init() {
+		this.serverOffsets = [];
 		this.serverOffset = TimeSync.serverOffset();
+		this.serverOffsets.push(this.serverOffset);
 		this.timeSyncTimeout = Meteor.setInterval(() => {
 			TimeSync.resync();
 			this.serverOffset = TimeSync.serverOffset();
+			this.serverOffsets.push(this.serverOffset);
 		}, 2500);
 	}
 
@@ -19,8 +22,9 @@ export default class ServerNormalizedTime {
 	getServerTimestamp() {
 		let serverOffset = 0;
 
-		if (this.serverOffset !== undefined) {
-			serverOffset = this.serverOffset;
+		if (this.serverOffsets.length) {
+			const sum = this.serverOffsets.reduce((previous, current) => current += previous);
+			serverOffset = sum / this.serverOffsets.length;
 		}
 
 		return getUTCTimeStamp() + serverOffset;
