78fca3cec8f53dc93a53c990440ec1e2095ea2b6
clesquir <clesquir@devalto.com>
2018-01-01 19:16:25 -0500

    Desktop controller events were not removed

--- imports/api/games/client/deviceController/DesktopController.js
+++ imports/api/games/client/deviceController/DesktopController.js
@@ -8,30 +8,35 @@ export default class DesktopController extends DeviceController {
 		super();
 		this.keymaps = keymaps;
 		this.keys = {left: null, right: null, up: null, down: null};
+		this.monitoringStarted = false;
 		this.onKeyDown = (event) => {
+			event.preventDefault();
 			return this.processKeyDown(event);
 		};
 		this.onKeyUp = (event) => {
+			event.preventDefault();
 			return this.processKeyUp(event);
 		};
 	}
 
 	startMonitoring() {
+		this.monitoringStarted = true;
 		window.addEventListener('keydown', this.onKeyDown, true);
 		window.addEventListener('keyup', this.onKeyUp, true);
 	}
 
 	stopMonitoring() {
-		window.removeEventListener('keydown', this.onKeyDown);
-		window.removeEventListener('keyup', this.onKeyUp);
+		if (this.monitoringStarted) {
+			window.removeEventListener('keydown', this.onKeyDown, true);
+			window.removeEventListener('keyup', this.onKeyUp, true);
+			this.monitoringStarted = false;
+		}
 	}
 
 	processKeyDown(event) {
 		const keyCode = event.keyCode;
 
 		if (this.keymaps.monitors(keyCode)) {
-			event.preventDefault();
-
 			const mapping = this.keymaps.mappingForKeyCode(keyCode);
 			this.keys[mapping] = keyCode;
 		}
@@ -41,8 +46,6 @@ export default class DesktopController extends DeviceController {
 		const keyCode = event.keyCode;
 
 		if (this.keymaps.monitors(keyCode)) {
-			event.preventDefault();
-
 			const mapping = this.keymaps.mappingForKeyCode(keyCode);
 			this.keys[mapping] = null;
 		}
