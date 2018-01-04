f865d8e4afab6f23ff6d878d9ff326d6a8dd31ea
Cedric Lesquir <clesquir@devalto.com>
2018-01-04 12:40:27 -0500

    Fixed tournament name not overridden in user profile

--- imports/ui/pages/tournamentUserProfile.js
+++ imports/ui/pages/tournamentUserProfile.js
@@ -3,6 +3,14 @@ import {Template} from 'meteor/templating';
 import './tournamentUserProfile.html';
 
 Template.tournamentUserProfile.helpers({
+	tournamentName: function() {
+		if (this.tournament.name) {
+			return this.tournament.name;
+		}
+
+		return this.tournament.mode.name;
+	},
+
 	tournamentDescription: function() {
 		if (this.tournament.description) {
 			return this.tournament.description;
