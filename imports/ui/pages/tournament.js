f64d2e8b4000dbdc118c92c20a5cd2c73bb6e24d
Cédric Lesquir <clesquir@devalto.com>
2018-01-23 21:48:28 -0500

    Add line chart in tournaments (#150)

--- imports/ui/pages/tournament.js
+++ imports/ui/pages/tournament.js
@@ -1,16 +1,23 @@
-import {Meteor} from 'meteor/meteor';
-import {Template} from 'meteor/templating';
-import {Session} from 'meteor/session';
-import {Router} from 'meteor/iron:router';
+import RankChart from '/imports/api/ranks/client/RankChart.js';
 import {Tournaments} from '/imports/api/tournaments/tournaments.js';
 import {canPlayTournament, isTournamentActive} from '/imports/api/tournaments/utils.js';
 import CardSwitcher from '/imports/lib/client/CardSwitcher.js';
 import {loadStatistics} from '/imports/ui/views/statistics.js';
+import {timeElapsedSince} from '/imports/lib/utils.js';
+import {Router} from 'meteor/iron:router';
+import {Meteor} from 'meteor/meteor';
+import * as Moment from 'meteor/momentjs:moment';
+import {Mongo} from "meteor/mongo";
+import {Session} from 'meteor/session';
+import {Template} from 'meteor/templating';
 
 import './tournament.html';
 
 class TournamentRankingsCollection extends Mongo.Collection {}
 const TournamentRankings = new TournamentRankingsCollection('tournamentrankings');
+
+class RankChartCollection extends Mongo.Collection {}
+const RankChartData = new RankChartCollection('tournamentrankchartdata');
 
 Template.tournament.onCreated(function() {
 	this.autorun(() => {
@@ -27,9 +34,21 @@ Template.tournament.onRendered(function() {
 			'tournament-games': TournamentViews.viewTournamentGames,
 			'tournament-statistics': TournamentViews.viewTournamentStatistics,
 			'tournament-rank': TournamentViews.viewTournamentRank,
+			'tournament-line-chart': TournamentViews.viewTournamentLineChart,
 		}
 	);
 });
+
+/** @type {RankChart}|null */
+let rankChart = null;
+let rankChartSubscriptionHandler = null;
+
+Template.tournament.destroyed = function() {
+	rankChart = null;
+	if (rankChartSubscriptionHandler) {
+		rankChartSubscriptionHandler.stop();
+	}
+};
 
 Template.tournament.helpers({
 	tournamentName: function() {
@@ -114,6 +133,16 @@ Template.tournament.events({
 		}
 	},
 
+	'click [data-action=view-tournament-line-chart]': function() {
+		const tournament = Tournaments.findOne({_id: Session.get('tournament')});
+
+		if (isTournamentActive(tournament)) {
+			cardSwitcher.slideTo(3);
+		} else {
+			cardSwitcher.slideTo(2);
+		}
+	},
+
 	'click [data-action=create-tournament-game]': function() {
 		Tooltips.hide();
 		Session.set('appLoadingMask', true);
@@ -159,9 +188,46 @@ class TournamentViews {
 		}
 	}
 
+	static viewTournamentLineChart() {
+		const tournamentContents = document.getElementById('tournament-contents');
+
+		if (!$(tournamentContents).is('.tournament-line-chart-shown')) {
+			TournamentViews.removeShownClasses(tournamentContents);
+			$(tournamentContents).addClass('tournament-line-chart-shown');
+			updateRankChart();
+		}
+	}
+
 	static removeShownClasses(tournamentContents) {
 		$(tournamentContents).removeClass('tournament-statistics-shown');
 		$(tournamentContents).removeClass('tournament-games-shown');
 		$(tournamentContents).removeClass('tournament-rank-shown');
+		$(tournamentContents).removeClass('tournament-line-chart-shown');
 	}
 }
+
+const updateRankChart = function() {
+	Session.set('lineChartDisplayLoadingMask', true);
+
+	if (!rankChart) {
+		rankChart = new RankChart(
+			'rank-line-chart-canvas',
+			RankChartData.find({}, {sort: ['timestamp']})
+		);
+	}
+
+	if (rankChartSubscriptionHandler) {
+		rankChartSubscriptionHandler.stop();
+	}
+	rankChartSubscriptionHandler = Meteor.subscribe('tournamentRanksChart', Session.get('tournament'), () => {
+		const tournament = Tournaments.findOne({_id: Session.get('tournament')});
+
+		if (!tournament) {
+			return;
+		}
+
+		const date = Moment.moment(tournament.startDate, 'YYYY-MM-DD ZZ');
+		rankChart.update(timeElapsedSince(date.valueOf()));
+		Session.set('lineChartDisplayLoadingMask', false);
+	});
+};
