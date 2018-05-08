ceb326de18bdd58ae4e4a06ccf2c9fdd5485846b
clesquir <clesquir@devalto.com>
2018-05-08 18:27:12 -0400

    Removed applied migrations

--- imports/api/achievements/server/migrations/updateAchievements.js
+++ imports/api/achievements/server/migrations/updateAchievements.js
@@ -1,36 +1,4 @@
-import {Achievements} from '/imports/api/achievements/achievements.js';
-import {
-	ACHIEVEMENT_BATTLE_OF_THE_GIANTS,
-	ACHIEVEMENT_BLANK_SCREEN,
-	ACHIEVEMENT_DAVID_VS_GOLIATH,
-	ACHIEVEMENT_FULL_STOP,
-	ACHIEVEMENT_I_WAS_THERE_WAITING,
-	ACHIEVEMENT_TEENY_TINY_WORLD
-} from '/imports/api/achievements/constants.js';
 import {Meteor} from 'meteor/meteor';
 
 Meteor.startup(function() {
-	/**
-	 * Migration for updating achievements
-	 */
-	updateDeniedForTwoVersusTwoGame(ACHIEVEMENT_BATTLE_OF_THE_GIANTS);
-	updateDeniedForTwoVersusTwoGame(ACHIEVEMENT_I_WAS_THERE_WAITING);
-	updateDeniedForTwoVersusTwoGame(ACHIEVEMENT_BLANK_SCREEN);
-	updateDeniedForTwoVersusTwoGame(ACHIEVEMENT_DAVID_VS_GOLIATH);
-	updateDeniedForTwoVersusTwoGame(ACHIEVEMENT_FULL_STOP);
-	updateDeniedForTwoVersusTwoGame(ACHIEVEMENT_TEENY_TINY_WORLD);
 });
-
-const updateDeniedForTwoVersusTwoGame = function(id) {
-	const achievement = Achievements.findOne({_id: id});
-	if (achievement !== undefined && !achievement.deniedForTwoVersusTwo) {
-		Achievements.update(
-			{_id: id},
-			{
-				$set: {
-					deniedForTwoVersusTwo: true
-				}
-			}
-		);
-	}
-};
--- imports/api/games/server/migrations/updateGames.js
+++ imports/api/games/server/migrations/updateGames.js
@@ -1,25 +1,4 @@
-import {ONE_VS_ONE_GAME_MODE} from '/imports/api/games/constants.js';
-import {Games} from '/imports/api/games/games.js';
 import {Meteor} from 'meteor/meteor';
 
 Meteor.startup(function() {
-	/**
-	 * Migration for updating game
-	 */
-	Games.find({players: {$exists: false}}).forEach(
-		function (game) {
-			const players = [];
-
-			if (game.hostId) {
-				players.push({id: game.hostId, name: game.hostName});
-			}
-			if (game.clientId) {
-				players.push({id: game.clientId, name: game.clientName});
-			}
-
-			Games.update({_id: game._id}, {$set: {players: players}});
-		}
-	);
-	Games.update({gameMode: {$exists: false}}, {$set: {gameMode: ONE_VS_ONE_GAME_MODE}}, {multi: true});
-	Games.update({hostId: {$exists: true}}, {$unset: {hostId: 1, hostName: 1, clientId: 1, clientName: 1}}, {multi: true});
 });
