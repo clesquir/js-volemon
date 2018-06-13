a8f4853f1d97f9445edfe1eec85b04496f001473
clesquir <clesquir@devalto.com>
2018-06-13 08:45:26 -0400

    Space skin

--- imports/api/skins/skinConstants.js
+++ imports/api/skins/skinConstants.js
@@ -3,3 +3,4 @@ export const SKIN_DEVALTO = 'devalto';
 export const SKIN_MARIO_BROS = 'mario-bros';
 export const SKIN_JUJU_WORLD = 'juju-world';
 export const SKIN_INDUSTRIAL = 'industrial';
+export const SKIN_SPACE = 'space';
