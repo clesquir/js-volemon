26fc3ddb0b2388da44cb87911745ffff5d6bfd89
clesquir <clesquir@devalto.com>
2018-12-29 15:23:40 -0500

    Unique names for CPU and ML CPU

--- imports/api/games/server/matchMaking/ImmediateMatchMaker.js
+++ imports/api/games/server/matchMaking/ImmediateMatchMaker.js
@@ -33,7 +33,7 @@ export default class ImmediateMatchMaker extends MatchMaker {
 			case ONE_VS_COMPUTER_GAME_MODE:
 				return [match.usersToMatch[0], {id: 'CPU', name: 'CPU'}];
 			case ONE_VS_MACHINE_LEARNING_COMPUTER_GAME_MODE:
-				return [match.usersToMatch[0], {id: 'CPU', isMachineLearning: true, name: 'CPU'}];
+				return [match.usersToMatch[0], {id: 'CPU', isMachineLearning: true, name: 'ML CPU'}];
 			case ONE_VS_ONE_GAME_MODE:
 				if (match.usersToMatch.length === 2) {
 					return match.usersToMatch;
