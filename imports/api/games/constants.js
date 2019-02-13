710599e4d6c5a079318203b74b8698015200fe47
clesquir <clesquir@devalto.com>
2019-02-13 08:39:06 -0500

    Phaser 3 upgrade
    - bonus mass now is same as ball mass

--- imports/api/games/constants.js
+++ imports/api/games/constants.js
@@ -25,7 +25,7 @@ export const BALL_AIR_FRICTION = 0;
 
 export const BONUS_INDICATOR_RADIUS = 15;
 export const BONUS_RADIUS = 17;
-export const BONUS_MASS = 0.36;
+export const BONUS_MASS = 0.1;
 export const BONUS_SCALE = 1;
 export const BONUS_AIR_FRICTION = 0;
 
